c4945d8fad6958afc93d3ce8c5f214411916e94f
jest-games-llc <jest.games.llc@gmail.com>
2023-08-01 20:32:34 -0400

    [v1.0.2] Add object to generics of XOR.

--- src/types/XOR.ts
+++ src/types/XOR.ts
@@ -1,3 +1,5 @@
 import { Without } from '.';
 
-export type XOR<T, U> = T | U extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
+export type XOR<T extends object, U extends object> = T | U extends object
+  ? (Without<T, U> & U) | (Without<U, T> & T)
+  : T | U;
